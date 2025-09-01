272fa84ac2429cff54a354f5905512d9c12100ad
userofghb <163718459+userofghb@users.noreply.github.com>
2025-09-01 15:43:33 +0800

    Update Main.java

--- myvcampus/server/src/main/java/appserver/Main.java
+++ myvcampus/server/src/main/java/appserver/Main.java
@@ -3,7 +3,14 @@ package appserver;
 import appserver.net.Server;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        new Server(8000).run();
+    public static void main(String[] args) {
+        int port = 8000; // 默认端口
+        try {
+            Server server = new Server(port);
+            server.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("服务器启动失败！");
+        }
     }
 }
